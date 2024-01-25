c0a5c8a76a99c28af76519792432a9a08261cef4
sayedhanzala <sayedhanzala123@gmail.com>
2024-01-25 17:47:19 +0530

    first commit

---
+++ app1/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <h1>Learing React</h1>
+      <br />
+      <p>this is para</p>
+    </>
+  );
+}
+
+export default App;
